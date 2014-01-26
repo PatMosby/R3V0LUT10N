0f294695322104f9a3ee4d80f667e8fcf42f29c4
Patrick Damrow <damsen@tzi.de>
2014-01-26 14:27:47 +0100

    damn

--- BibR3V/bibjsf/src/main/java/swp/bibjsf/persistence/Persistence.java
+++ BibR3V/bibjsf/src/main/java/swp/bibjsf/persistence/Persistence.java
@@ -382,12 +382,7 @@ public interface Persistence {
 	 */
 	public void restore() throws DataSourceException;
 	
-<<<<<<< HEAD
 	public void addLending(int bookID, int readerID, Date date, double charges) throws DataSourceException, SQLException;
-=======
-	public void addLending(Borrower borrower, Date date) throws DataSourceException,
-	BusinessElementAlreadyExistsException ;
->>>>>>> b00fb14432e8e3357772dae258082afdb130d526
 	
 	public void addLendings(String bookIDs, int readerID, String dates) throws DataSourceException;
 	
