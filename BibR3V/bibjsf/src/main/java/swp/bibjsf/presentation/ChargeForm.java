240423c951499c7c9516752f3abe8fc2a5e2731a
sbrede <sbrede@informatik.uni-bremen.de>
2014-01-26 22:08:11 +0100

    bsjabfda

---
+++ BibR3V/bibjsf/src/main/java/swp/bibjsf/presentation/ChargeForm.java
@@ -0,0 +1,30 @@
+package swp.bibjsf.presentation;
+
+import java.util.Date;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import swp.bibcommon.Charges;
+import swp.bibjsf.businesslogic.BorrowHandler;
+import swp.bibjsf.utils.Messages;
+
+public abstract class ChargeForm extends BusinessObjectForm<Charges>{
+
+	
+	private static final long serialVersionUID = 380665113958410405L;
+
+	
+	
+	public abstract String getTyp();
+
+    public abstract void  setTyp(final String typ);
+        
+    public abstract Double getCharges();
+          
+    public abstract void setCharges(final Double charges);   	
+
+}
