0002a4f369e5df7057bf4ff620bcec5d786968b5
pupat <s_aydi4h@uni-bremen.de>
2014-02-23 14:08:55 +0100

    statistik

--- BibR3V/bibjsf/src/main/java/swp/bibjsf/presentation/AddBorrowerForm.java
+++ BibR3V/bibjsf/src/main/java/swp/bibjsf/presentation/AddBorrowerForm.java
@@ -41,8 +41,8 @@ public class AddBorrowerForm extends BorrowerForm{
 						logger.debug("Anomalie MediumID: "+borrower.getBookID() );
 					}
 	    			    	
-//	    	          BookHandler boh = BookHandler.getInstance();
-//	    			  boh.updateStatistik(borrower.getBookID());
+	    	          BookHandler boh = BookHandler.getInstance();
+	    			  boh.updateStatistik(borrower.getBookID());
 	    			  
 	    			  return success(bh.add(borrower));
 	    			
